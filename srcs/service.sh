5b45ea8f9cd4220b47695ca7f3c3edb157a50661
Alae Ez-zaouine <aez-zaou@e2r5p8.1337.ma>
2020-12-28 16:01:45 +0100

    delete unwanted files

--- srcs/service.sh
+++ srcs/service.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 rm /etc/nginx/sites-available/default
 mv /default /etc/nginx/sites-available/
-echo "<?php phpinfo( ); ?>" >> /var/www/html/phpinfo.php
-echo "<h1>helloooo</h1>" >> /var/www/html/alae.html
 service nginx restart
 service mysql restart
 service php7.3-fpm stop && service php7.3-fpm start
