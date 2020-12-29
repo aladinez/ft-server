84fc91cae277f736457f72c25edd3e73ba655d28
Alae Ez-zaouine <aez-zaou@e2r5p8.1337.ma>
2020-12-29 11:34:31 +0100

    set autoindex off, added a new databse for phpmyadmin

--- srcs/mysql.sh
+++ srcs/mysql.sh
@@ -8,4 +8,5 @@ pass="admin"
 mysql -u $user -e "CREATE DATABASE $dbname;CREATE USER $name@localhost identified by '$pass';GRANT ALL ON *.* to $name@localhost WITH GRANT OPTION;"
 #grant all tables on all databases to $name , with ability to grant any other user (with grant optin).
 
+mysql < /usr/share/phpmyadmin/sql/create_tables.sql -u root
 mysql < db_wordpress.sql -u root db_wordpress
